1d0046cf0450d2103cf9c96ed7578077a6598c19
Uriel Herrera <contactoherrerauriel@gmail.com>
2023-07-11 15:24:03 -0300

    commit inicial

---
+++ routes/api.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+
+router.post('/contacto', async (req, res) =>{
+    const mail = {
+        to: 'contactoherrerauriel@gmail.com',
+        subject: 'Contacto web',
+        html: `${req.body.nombre} se contacto a través de la web y quiere más información a este correo: ${req.body.email} <br> Además, hizo el siguiente comentario: ${req.body.mensaje} <br> Su tel es: ${req.body.telefono}`
+    }
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+    await transport.sendMail(mail)
+
+    res.status(201).json({
+        error: false,
+        message: 'Mensaje enviado'
+    });
+});
+
+module.exports = router;
